f939670e0e231aea5f256e635605c8b9f6735ff7
ZaLeep <lexa35695@gmail.com>
2023-06-19 18:37:50 +0300

    Fixed ItemPage going back

--- GmeRecomendationSystem/Controllers/ItemPageController.cs
+++ GmeRecomendationSystem/Controllers/ItemPageController.cs
@@ -2,34 +2,39 @@
 using GmeRecomendationSystem.Utils;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using web = System.Web;
 
 namespace GmeRecomendationSystem.Controllers
 {
 	[Authorize]
+    [ApiController]
 	[Route("[controller]")]
 	public class ItemPageController : Controller
 	{
 		[Route("Index/{Id}")]
 		public  IActionResult Index(int Id)
         {
+            ViewData["Prev"] = Request.Headers["Referer"].ToString();
             HomeController.UserContext(User.Identity as ClaimsIdentity, ViewData);
             return View( DBWork.GetItem(Id, Convert.ToInt32(ViewData["4"]), Convert.ToInt32(ViewData["0"])));
         }
 
         [Route("AddReview/{Id}")]
-        public  IActionResult AddReview(int Id, int score)
+        public IActionResult AddReview(int Id, int score)
         {
+            ViewData["Prev"] = Request.Headers["Referer"].ToString();
             HomeController.UserContext(User.Identity as ClaimsIdentity, ViewData);
             DBWork.SetCheckedItem(Id, Convert.ToInt32(ViewData["0"]), Convert.ToInt32(ViewData["4"]), score);
-            return View("Index",  DBWork.GetItem(Id, Convert.ToInt32(ViewData["4"]), Convert.ToInt32(ViewData["0"])));
+            return View("Index", DBWork.GetItem(Id, Convert.ToInt32(ViewData["4"]), Convert.ToInt32(ViewData["0"])));
         }
 
         [Route("DeleteReview/{Id}")]
-        public  IActionResult DeleteReview(int Id)
+        public IActionResult DeleteReview(int Id)
         {
+            ViewData["Prev"] = Request.Headers["Referer"].ToString();
             HomeController.UserContext(User.Identity as ClaimsIdentity, ViewData);
             DBWork.UnsetCheckedItem(Id, Convert.ToInt32(ViewData["0"]), Convert.ToInt32(ViewData["4"]));
-            return View("Index",  DBWork.GetItem(Id, Convert.ToInt32(ViewData["4"]), Convert.ToInt32(ViewData["0"])));
+            return View("Index", DBWork.GetItem(Id, Convert.ToInt32(ViewData["4"]), Convert.ToInt32(ViewData["0"])));
         }
     }
 }
